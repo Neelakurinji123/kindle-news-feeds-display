9a4a13355be1d6c53218c2126936f6819713a7d5
Neelakurinji123 <70471447+Neelakurinji123@users.noreply.github.com>
2020-09-06 19:32:21 +0900

    update

--- kindle/kindle-news-feeds/script.sh
+++ kindle/kindle-news-feeds/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#cd "$(dirname "$0")"
+cd "$(dirname "$0")"
 
 pidof powerd >/dev/null
 if [ $? -eq 0 ]; then
@@ -11,7 +11,6 @@ fi
 rm -f /tmp/news_feeds.tar
 wget -q http://192.168.2.1:8080/news_feeds.tar -O /tmp/news_feeds.tar
 
-
 [ -d /tmp/news-feeds ] || mkdir -p /tmp/news-feeds
 [ -f /tmp/news-feeds/control.env ] && rm /tmp/news-feeds/*
 
@@ -19,17 +18,27 @@ tar xf /tmp/news_feeds.tar -C /tmp/news-feeds
 
 . /tmp/news-feeds/control.env
 
-eips -c
-count=0
-while [ $count -lt $repeat ]; do
-    count=`expr $count + 1`
-    for n in `echo /tmp/news-feeds/entry*.png`; do
-        eips -g $n
-        sleep $duration_time
-        if [ $display_reset = 'True' ]; then
-            eips -c
-        elif [ $display_reset = 'False' ]; then
-            continue
-        fi
+run()
+{
+    count=0
+    while [ $count -lt $repeat ]; do
+        count=`expr $count + 1`
+        for n in `echo /tmp/news-feeds/entry*.png`; do
+            eips -g $n
+            sleep $duration_time
+            if [ $display_reset = 'True' ]; then
+                eips -c
+            elif [ $display_reset = 'False' ]; then
+                continue
+            fi
+        done
     done
-done
+}
+
+eips -c
+if [ -f '/tmp/news-feeds/entry0.pngz' ]; then
+    run
+else
+    eips -g error.png
+fi
+
