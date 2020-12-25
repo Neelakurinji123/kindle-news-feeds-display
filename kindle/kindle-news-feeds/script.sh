879cd226124e11bcb6fe3048668f36e6440f724d
Neelakurinji123 <70471447+Neelakurinji123@users.noreply.github.com>
2020-12-26 05:15:30 +0900

    update

--- kindle/kindle-news-feeds/script.sh
+++ kindle/kindle-news-feeds/script.sh
@@ -2,43 +2,51 @@
 
 cd "$(dirname "$0")"
 
-#pidof powerd >/dev/null
-#if [ $? -eq 0 ]; then
-#    /etc/init.d/powerd stop
-#    /etc/init.d/framework stop
-#fi
+if [ -f /mnt/us/kindle-weather/enable ]; then
 
-rm -f /www/news_feeds.tar
-wget -q http://192.168.2.1:8080/news_feeds.tar -O /www/news_feeds.tar
+    if [ "`pidof powerd`" != '' ]; then
+        /etc/init.d/powerd stop
+        /etc/init.d/framework stop
+    fi
 
-[ -d /www/news-feeds ] || mkdir -p /www/news-feeds
-[ -f /www/news-feeds/control.env ] && rm -rf /www/news-feeds
+    if [ "`mount -l | grep 'tmpfs on /www'`" == '' ]; then
+        mount -a
+        sleep 3
+    fi
 
-tar xf /www/news_feeds.tar -C /www/news-feeds
+    rm -f /www/news_feeds.tar
+    wget -q http://192.168.2.1:8080/news_feeds.tar -O /www/news_feeds.tar
 
-. /www/news-feeds/control.env
+    [ -d /www/news-feeds ] || mkdir -p /www/news-feeds
+    [ -f /www/news-feeds/control.env ] && rm -rf /www/news-feeds
 
-run()
-{
-    count=0
-    while [ $count -lt $repeat ]; do
-        count=`expr $count + 1`
-        for n in `echo /www/news-feeds/entry*.png`; do
-            eips -g $n
-            sleep $duration_time
-            if [ $display_reset = 'True' ]; then
-                eips -c
-            elif [ $display_reset = 'False' ]; then
-                continue
-            fi
+    tar xf /www/news_feeds.tar -C /www/news-feeds
+
+    . /www/news-feeds/control.env
+
+    run()
+    {
+        count=0
+        while [ $count -lt $repeat ]; do
+            count=`expr $count + 1`
+            for n in `echo /www/news-feeds/entry*.png`; do
+                eips -g $n
+                sleep $duration_time
+                if [ $display_reset = 'True' ]; then
+                    eips -c
+                elif [ $display_reset = 'False' ]; then
+                    continue
+                fi
+            done
         done
-    done
-}
+    }
 
-eips -c
-if [ -f '/www/news-feeds/entry0.png' ]; then
-    run
-else
-    eips -g error.png
+    eips -c
+
+    if [ -f '/www/news-feeds/entry0.png' ]; then
+        run
+    else
+        eips -g error.png
+    fi
 fi
 
